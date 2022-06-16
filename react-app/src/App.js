11f4235b7f1bf7c92e2244a29cd95c01d40316f4
MajASAR <majkrav@asardigital.com>
2022-06-16 11:07:27 -0500

    Added side navbar with collapsible items.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -17,7 +17,7 @@ import Dashboard from './components/DashboardPage';
 import { authenticate } from './store/session';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ManageCustomers from './components/ManageCustomersPage';
-
+import SideNav from './components/SideNav/index.js';
 
 
 function App() {
@@ -38,6 +38,7 @@ function App() {
   return (
     <BrowserRouter>
       <NavBar />
+      {/* <SideNav/> */}
       <Switch>
         <Route path='/login' exact={true}>
           <LoginPage />
