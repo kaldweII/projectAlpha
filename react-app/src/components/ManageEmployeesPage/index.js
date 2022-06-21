becb1fecf98c755df5c551c2a091c12888e43373
MajASAR <majkrav@asardigital.com>
2022-06-21 12:03:27 -0500

    Added page viewing permissions based on role.

--- react-app/src/components/ManageEmployeesPage/index.js
+++ react-app/src/components/ManageEmployeesPage/index.js
@@ -2,23 +2,31 @@ import '../View.css'
 import asarLogo from '../../assets/asarLogo.png'
 import AddEmployee from '../Employee/AddEmployee';
 import EmployeeList from '../Employee/EmployeeList';
+import { useSelector } from 'react-redux';
 
 function ManageEmployees() {
+    const userRole = useSelector(state => state.session.user.role)
 
-    return (
-        <div className="dashboard">
-            <div className="dashBkg"></div>
-            <div className="dashHeaderContainer">
-                <h1 className="dashHeading">Manage Employees</h1>
-            </div>
-            <AddEmployee />
+    if (userRole === 'admin') {
+        return (
+            <div className="dashboard">
+                <div className="dashBkg"></div>
+                <div className="dashHeaderContainer">
+                    <h1 className="dashHeading">Manage Employees</h1>
+                </div>
+                <AddEmployee />
 
-            <div className="table">
-                <EmployeeList />
+                <div className="table">
+                    <EmployeeList />
+                </div>
+                <img className='companyLogo' src={asarLogo}></img>
             </div>
-            <img className='companyLogo' src={asarLogo}></img>
-        </div>
-    )
+        );
+    } else {
+        return (
+            <div className='dashboard notAuthorized'><p>Error: You are not authorized to view this page.</p></div>
+        );
+    }
 }
 
 export default ManageEmployees;
